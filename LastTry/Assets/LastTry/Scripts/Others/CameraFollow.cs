b734e14c99b33ee3cedc2c5508726f787ed10eeb
Kamran Wali <kamran369mars@gmail.com>
2019-09-24 17:45:38 +0100

    Created CameraFollow
    Created the camera follow class. Now the camera follows the player smoothly, disabled the 'look at player' feature because it feels dizzy to look at.

---
+++ LastTry/Assets/LastTry/Scripts/Others/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Camera Follow Properties")]
+    public Transform Target;
+    [Range(0.0f, 1.0f)]
+    public float SpeedSlerp;
+
+    private Vector3 _cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setting up the camera offset position
+        _cameraOffset = transform.position - Target.position;
+    }
+
+    // LateUpdate is called after Update methods
+    void LateUpdate()
+    {
+        // Moving the camera
+        transform.position = Vector3.Slerp(transform.position,
+                                           Target.position + _cameraOffset,
+                                           SpeedSlerp);
+
+        // This is for looking at the player
+        // transform.LookAt(Target); // Looking at the player
+    }
+}
