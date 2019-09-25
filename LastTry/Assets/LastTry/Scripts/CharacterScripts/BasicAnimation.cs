e3fd3851759dc177d6346c3a4325907a23dbcfbc
Kamran Wali <kamran369mars@gmail.com>
2019-09-25 17:12:29 +0100

    Updated BasicAnimation
    The BasicAnimation now plays the idle, walk and run animation when the character is moving. Using values from [0,1] for showing the animations. Made sure the values are absolute as well.

--- LastTry/Assets/LastTry/Scripts/CharacterScripts/BasicAnimation.cs
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/BasicAnimation.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 /// </summary>
 public class BasicAnimation : BasicCharacter
 {
+    private readonly string AnimationMovePercentage = "MovePercentage";
+
+    [Header("Basic Animation Properties")]
+    public Animator CharacterAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +24,41 @@ public class BasicAnimation : BasicCharacter
     protected override void InitializeStartUp()
     {
         base.InitializeStartUp();
+    }
+
+    /// <summary>
+    /// This method sets the move percentage of the move animation.
+    /// </summary>
+    /// <param name="percentage">The move percentage value between 0.0 - 1.0f,
+    ///                          of type float</param>
+    protected void SetMoveSpeed(float percentage)
+    {
+        // Converting percentage value to absolute value
+        percentage = percentage < 0 ? percentage * -1 : percentage;
+        CharacterAnimator.SetFloat(AnimationMovePercentage, percentage);
+    }
+
+    /// <summary>
+    /// This method sets the highest value from two values as the
+    /// the move percentage of the move animation.
+    /// </summary>
+    /// <param name="value1">First value to compare, of type float</param>
+    /// <param name="value2">Second value to compare, of type float</param>
+    protected void SetMoveSpeed(float value1, float value2)
+    {
+        // Converting value1 value to absolute value
+        value1 = value1 < 0 ? value1 * -1 : value1;
+        // Converting percentage value to absolute value
+        value2 = value2 < 0 ? value2 * -1 : value2;
+
+        // When player not moving that is both value1 and value2
+        // are zero which is the idle animation
+        if (value1 == 0 && value2 == 0) SetMoveSpeed(0);
+
+        // Taking the value1 value to apply as percentage
+        else if (value1 > value2) SetMoveSpeed(value1);
+
+        // Taking the value2 value to apply as percentage
+        else SetMoveSpeed(value2); // Vertical value
     }
 }
