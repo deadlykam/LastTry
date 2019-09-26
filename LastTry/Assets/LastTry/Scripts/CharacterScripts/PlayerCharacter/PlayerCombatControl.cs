58632f695ca97c9f1aada6ee75596168efd69742
Kamran Wali <kamran369mars@gmail.com>
2019-09-27 00:37:37 +0100

    Created PlayerCombatControl
    This class handles the combat system of the player. This class can be made into a combo system later if needed. For now this class makes sure that when repeated attack button is pressed it will rightfully take in the attack input and then execute it. This class also extends BasicAnimation so it also calls the attack animation when attack is called.
    In future damage to enemies may be given from here.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/PlayerCharacter/PlayerCombatControl.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatControl : BasicAnimation
+{
+    public enum CombatState { None, SetupInput, Processing, AcceptInput };
+
+    [Header("Player Combat Control Properties")]
+    public AttackAnimationInfo[] AnimationInfos;
+
+    // For checking if input is allowed to be accepted
+    public bool IsAcceptInput { get { return _combatState == CombatState.AcceptInput; } }
+
+    // For checking if the player is not in attack mode
+    public bool IsMovable { get { return !_isCurrentCombatProcessing &&
+                                         _combatInputs.Count == 0; } }
+
+    private Queue<CombatInfo> _combatInputs = new Queue<CombatInfo>();
+    private CombatState _combatState = CombatState.AcceptInput;
+    private CombatInfo _currentCombatInfo; // The current combat time information
+                                           // of the animation
+
+    private bool _isCurrentCombatProcessing; // For checking if current CombatInfo
+                                             // is being processed
+
+    private bool _isAcceptInputThreshold; // For checking if the current CombatInfo
+                                          // have crossed the threshold for accepting
+                                          // new inputs
+
+    private float _currentAnimationTime; // Animation time for the current combat animation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeStartUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerCombatControlUpdate();
+    }
+    
+    /// <summary>
+    /// This method sets up the new combat input to be processed.
+    /// </summary>
+    private void SetupCombatInputForProcessing()
+    {
+        // Condition for adding a new input to be processed
+        if (_combatInputs.Count != 0 && !_isCurrentCombatProcessing
+            && _combatState == CombatState.SetupInput)
+        {
+            _currentCombatInfo = _combatInputs.Dequeue(); // Getting the next CombatInfo
+            _combatState = CombatState.Processing; // Start the processing of the input
+            _isCurrentCombatProcessing = true; // Current CombatInfo is being processed
+            _isAcceptInputThreshold = false; // Accepting input threshold resetted
+            _currentAnimationTime = 0; // Resetting current animation timer for further use
+            PlayAttackAnimation(_currentCombatInfo.Weapon); // Playing the current
+                                                            // attack animation
+        }
+
+        // Todo: Check if else condition is require as fail safe here
+    }
+
+    /// <summary>
+    /// This method processes the current CombatInfo.
+    /// </summary>
+    private void ProcessingCombatInfo()
+    {
+        // Condition for processing the curent CombatInfo
+        if (_isCurrentCombatProcessing)
+        {
+            // The current time of the combat animation
+            _currentAnimationTime += Time.deltaTime;
+
+            // Condition for accepting a new combat input
+            if(_currentAnimationTime >= _currentCombatInfo.ProcessTime &&
+               !_isAcceptInputThreshold)
+            {
+                _combatState = CombatState.AcceptInput; // New combat input can be accepted
+                _isAcceptInputThreshold = true; // Accepting input threshold crossed
+            }
+
+            // Condition for finishing the process of the current CombatInfo
+            if(_currentAnimationTime >= _currentCombatInfo.TotalTime)
+            {
+                _isCurrentCombatProcessing = false; // Processing done for current 
+                                                    // CombatInfo
+            }
+        }
+    }
+
+    /// <summary>
+    /// This method initializes the player combat control at the start up.
+    /// </summary>
+    protected override void InitializeStartUp()
+    {
+        base.InitializeStartUp();
+        //_combatInputs = new Queue<CombatInfo>();
+        //_combatState = CombatState.AcceptInput;
+        _isCurrentCombatProcessing = false;
+        _isAcceptInputThreshold = false;
+        _currentAnimationTime = 0;
+    }
+
+    /// <summary>
+    /// This method handles the player combat control update and must be called
+    /// by child classes for the processing of PlayerCombatControl.
+    /// </summary>
+    protected void PlayerCombatControlUpdate()
+    {
+        SetupCombatInputForProcessing();
+        ProcessingCombatInfo();
+    }
+
+    /// <summary>
+    /// This method adds a combat input to the queue to be processed.
+    /// </summary>
+    /// <param name="animationTimeInfo">The time information of the animation,
+    ///                                 of type CombatInfo</param>
+    protected void AddCombatInput(CombatInfo animationTimeInfo)
+    {
+        // Condition to check if input is allowed to be accepted
+        if (IsAcceptInput)
+        {
+            // Adding a new input to the queue
+            _combatInputs.Enqueue(animationTimeInfo);
+
+            // Starting to process the queue
+            _combatState = CombatState.SetupInput;
+        }
+    }
+}
+
+/// <summary>
+/// This class stores the time information of a combat animation.
+/// </summary>
+public struct CombatInfo
+{
+    /// <summary>
+    /// Total animation time.
+    /// </summary>
+    public readonly float TotalTime;
+
+    /// <summary>
+    /// The time at which no input will be accepted.
+    /// </summary>
+    public readonly float ProcessTime;
+
+    /// <summary>
+    /// The type of weapon animation to play.
+    /// </summary>
+    public readonly AttackAnimationInfo.WeaponType Weapon;
+
+    /// <summary>
+    /// This constructor creates an CombatInfo struct object.
+    /// </summary>
+    /// <param name="totalTime">The total time for the animation, of type float</param>
+    /// <param name="processTime">The time for which no input will be accepeted,
+    ///                           of type float</param>
+    /// <param name="weapon">The type of weapon animation to play, 
+    ///                      of type WeaponType</param>
+    public CombatInfo(float totalTime, float processTime, 
+                      AttackAnimationInfo.WeaponType weapon)
+    {
+        TotalTime = totalTime;
+        ProcessTime = processTime;
+        Weapon = weapon;
+    }
+}
