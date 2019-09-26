1a5fe08d17be59142cf3c85e39e04d480223dee4
Kamran Wali <kamran369mars@gmail.com>
2019-09-27 00:39:03 +0100

    Created AttackAnimationInfo
    This is a non-Monobehaviour class. It only stores information of an attack animation and the animation clip. It has been serialized so that it may be used in the editor.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/AttackAnimationInfo.cs
@@ -0,0 +1,19 @@
+﻿
+[System.Serializable]
+public class AttackAnimationInfo
+{
+    public enum WeaponType { None, Sword, Staff, Hammer};
+
+    public float TotalTime;
+    public float ProcessTime;
+    public WeaponType Weapon = WeaponType.None;
+
+    /// <summary>
+    /// This method converts this class to CombatInfo object.
+    /// </summary>
+    /// <returns>The converted CombatInfo, of type CombatInfo</returns>
+    public CombatInfo ToCombatInfo()
+    {
+        return new CombatInfo(TotalTime, ProcessTime, Weapon);
+    }
+}
