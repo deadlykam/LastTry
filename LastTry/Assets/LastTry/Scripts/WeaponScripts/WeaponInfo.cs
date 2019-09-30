fd3b0779bdf0d2f172b0d74729b6e966600f10cf
Kamran Wali <kamran369mars@gmail.com>
2019-09-30 17:43:42 +0100

    Created WeaponInfo
    Created the WeaponInfo class for keeping information of the weapon like, damage, cost, upgrade limit etc.

---
+++ LastTry/Assets/LastTry/Scripts/WeaponScripts/WeaponInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInfo : MonoBehaviour
+{
+    public enum WeaponType { None, Sword, Staff, Hammer};
+
+    public WeaponType Weapon;
+    public int Damage;
+    public int Cost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
