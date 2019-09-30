ccb8f604541e263854884a90501b6ba0aa308f05
Kamran Wali <kamran369mars@gmail.com>
2019-09-30 21:01:10 +0100

    Created EnemyManager
    For now the EnemyManager holds the PlayerCharacter info. Later this class will play a major role in populating the game world with enemies.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/EnemyCharacter/EnemyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("Enemy Manager Properties")]
+    public PlayerCharacter Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
