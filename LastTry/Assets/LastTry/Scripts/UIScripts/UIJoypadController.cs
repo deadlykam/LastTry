df73ecfbb1d0b266a2a678669bee7471e9df9a3d
Kamran Wali <kamran369mars@gmail.com>
2019-09-30 19:32:22 +0100

    Created UIJoypadController
    Created UIJoypadController which takes attack command from the screen and makes the player attack. Now the player can press the attack button for Attack. Tested it in android device(Samsung S7) and it works fine.

---
+++ LastTry/Assets/LastTry/Scripts/UIScripts/UIJoypadController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIJoypadController : MonoBehaviour
+{
+    [Header("UI Joypad Controller Properties")]
+    public PlayerCharacter Player;
+
+    /// <summary>
+    /// This method sends the button A command to the player.
+    /// </summary>
+    public void ButtonA() { Player.ButtonA(); }
+}
