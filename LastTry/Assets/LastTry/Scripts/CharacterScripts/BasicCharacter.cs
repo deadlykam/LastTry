cee3071661513f1e1381112d5fc8bf65d00baeaa
Kamran Wali <kamran369mars@gmail.com>
2019-09-30 17:45:39 +0100

    Updated BasicCharacter
    Added the Weapons attribute so that later in future will be able to add as much weapons as needed especially for the boss.

--- LastTry/Assets/LastTry/Scripts/CharacterScripts/BasicCharacter.cs
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/BasicCharacter.cs
@@ -11,6 +11,7 @@ public class BasicCharacter : MonoBehaviour
     public int HealthMax;
     public int HealthMin;
     private int _healthCur;
+    public WeaponInfo[] Weapons; // For storing multiple weapons for player and boss
 
     public float SpeedMovement;
 
@@ -21,10 +22,39 @@ public class BasicCharacter : MonoBehaviour
     }
 
     /// <summary>
-    /// This method initializes all basic attribute at the start up.
+    /// This method initializes all basic attribute at the start up in BasicCharacter.
     /// </summary>
     protected virtual void InitializeStartUp()
     {
         _healthCur = HealthMax; // Setting up the current health
+    }
+
+    /// <summary>
+    /// This method gets the default weapon of the character which is the
+    /// weapon at the 0th index.
+    /// </summary>
+    /// <returns>The weapon at the 0th index, of type WeaponInfo</returns>
+    protected WeaponInfo GetDefaultWeapon()
+    {
+        return Weapons[0];
+    }
+
+    /// <summary>
+    /// This method gets the default weapon type of the character which is
+    /// the weapontype at the 0th index.
+    /// </summary>
+    /// <returns>The weapon type at the 0th index, of type WeaponType</returns>
+    protected WeaponInfo.WeaponType GetDefaultWeaponType()
+    {
+        return GetDefaultWeapon().Weapon;
+    }
+
+    /// <summary>
+    /// This method picks up another weapon.
+    /// </summary>
+    protected virtual void PickUpWeapon()
+    {
+        //Todo: This method picks up a weapon and either replaces at the 0th index
+        //      or adds to 0th or 1st index.
     }
 }
