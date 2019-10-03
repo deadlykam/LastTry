52b6a5c8c674a388d6b2d00dc39dcca907e993f6
Kamran Wali <kamran369mars@gmail.com>
2019-10-03 18:16:19 +0100

    Created EnemyDetector
    This class detects the enemy and adds them to the player Enemy Detected list.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/EnemyDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    public PlayerCharacter Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Condition for adding the enemy to the weapon range
+        if (other.CompareTag("Enemy"))
+            Player.AddEnemyToRange(other.GetComponent<EnemyCharacter>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+            Player.RemoveEnemyFromRange(other.GetComponent<EnemyCharacter>());
+    }
+}
