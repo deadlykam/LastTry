26710bfc30b770d86f1c59ba886c38bcb6aec5cf
Kamran Wali <kamran369mars@gmail.com>
2019-10-03 23:07:37 +0100

    Created PlayerDetector
    Created a player detector class for detecting player when attacking. Needed for knowing if the player is within enemy weapon range.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/PlayerDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public EnemyCharacter Enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Enemy.SetPlayerInWeaponRange(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Enemy.SetPlayerInWeaponRange(false);
+    }
+}
