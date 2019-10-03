fafb3206cfe5ceaedb40aaea136d97917729fe7c
Kamran Wali <kamran369mars@gmail.com>
2019-10-03 23:15:18 +0100

    Created EnemyMelee
    This class is the first EnemyType class the Melee enemy. This class is made for enemies that does close combat attacks. The enemy will come close to the player and start to attack the player.
    
    NOTE: Since this model was not made for this project the attack animation does feel weird. Later create animations that will go well for the project.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/EnemyCharacter/EnemyMelee.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee : EnemyCharacter
+{
+    private bool _hasDealtDamage = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeStartUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calling the update of BasicCharacter
+        UpdateBasicCharacter();
+
+        // Calling the update of BasicAnimation
+        UpdateBasicAnimation();
+
+        // Calling the update of EnemyCharacter
+        UpdateEnemyCharacter();
+
+        UpdateEnemyMelee();
+    }
+
+    /// <summary>
+    /// This method initializes the enemy melee at the start up in EnemyMelee.
+    /// </summary>
+    protected override void InitializeStartUp()
+    {
+        base.InitializeStartUp();
+    }
+
+    /// <summary>
+    /// This method gets a new attack information from the EnemyCharacter and 
+    /// resets all attacking attributes.
+    /// </summary>
+    protected override void GetNewAttackAnimation()
+    {
+        base.GetNewAttackAnimation();
+        _hasDealtDamage = false;
+    }
+
+    /// <summary>
+    /// This method is the update method of EnemyMelee.
+    /// </summary>
+    protected void UpdateEnemyMelee()
+    {
+        if(!IsDead && !IsHurt) // Condition for when the enemy is not hurt or dead
+        {
+            if (IsReachedTarget && !IsAttacking) // Reached and attacking player
+            {
+                /*// Condition for getting a new attacking animation
+                // and attack information
+                if (!IsAttacking)
+                {
+                    GetNewAttackAnimation();
+                    PlayAttackAnimation(CurrentCombatInfo.AttackAnimation);
+                }*/
+
+                // Condition for starting to attack the player
+                if (IsPlayerInWeaponRange)
+                {
+                    GetNewAttackAnimation();
+                    PlayAttackAnimation(CurrentCombatInfo.AttackAnimation);
+                }
+            }
+
+            if (IsAttacking) // Condition for attacking
+            {
+                // Counting the attack animation timer
+                AttackTimer = (AttackTimer + Time.deltaTime)
+                               >= CurrentCombatInfo.TotalTime ?
+                               -1 : AttackTimer + Time.deltaTime;
+                
+                // Condition for dealing damage to the player
+                if (AttackTimer >= CurrentCombatInfo.ProcessTime &&
+                    !_hasDealtDamage)
+                {
+                    // Todo: Make sure the damage is taken from the weapon
+                    //       and not from a fixed value. Fixed value is
+                    //       only for testing.
+                    // Condition to make the player take damage
+                    if (IsPlayerInWeaponRange) Manager.Player.TakeDamage(5);
+                    _hasDealtDamage = true;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// This method makes the melee enemy take damage and resets any attack variables.
+    /// </summary>
+    /// <param name="amount"></param>
+    public override void TakeDamage(int amount)
+    {
+        base.TakeDamage(amount);
+        _hasDealtDamage = false;
+    }
+}
