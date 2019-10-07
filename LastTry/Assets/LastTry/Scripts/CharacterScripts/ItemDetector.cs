0e3bdb5b489d51b725f7cdc67c50413a4136d292
Kamran Wali <kamran369mars@gmail.com>
2019-10-07 19:25:33 +0100

    Created ItemDetector class
    This class is used only by the player to find items in the game world.

---
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/ItemDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDetector : MonoBehaviour
+{
+    public PlayerCharacter Player;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Checking and adding the weapon item
+        if (other.CompareTag("Weapons"))
+            Player.AddHoverWeapon(other.GetComponent<WeaponItem>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Condition for removing the hovered weapon
+        if (other.CompareTag("Weapons"))
+            Player.RemoveHoverWeapon(other.GetComponent<WeaponItem>());
+    }
+}
