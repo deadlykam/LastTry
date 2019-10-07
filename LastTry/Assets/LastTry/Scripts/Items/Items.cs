bb10e1365009ca28237378ffd07431531728e839
Kamran Wali <kamran369mars@gmail.com>
2019-10-07 19:26:46 +0100

    Created Items Class
    This class is added to item gameobjects so that the player can pick it up and get its attributes.

---
+++ LastTry/Assets/LastTry/Scripts/Items/Items.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Items : MonoBehaviour
+{
+    public string Title;
+    public int cost;
+
+    /// <summary>
+    /// This method returns the description of the item.
+    /// </summary>
+    /// <returns>The description of the item, of type string</returns>
+    public virtual string GetDescription()
+    {
+        return Title;
+    }
+}
