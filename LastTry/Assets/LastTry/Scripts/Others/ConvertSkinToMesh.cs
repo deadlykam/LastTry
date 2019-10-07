63da9d84210efb8c5ac0dc0dc5d68ef95d7813b7
Kamran Wali <kamran369mars@gmail.com>
2019-10-07 19:28:49 +0100

    Created ConvertSkinToMesh Class
    This class converts the skinned mesh renderer to normal renderer so that prefabs can be made of those skinned mesh renderer.

---
+++ LastTry/Assets/LastTry/Scripts/Others/ConvertSkinToMesh.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConvertSkinToMesh : MonoBehaviour
+{
+    /// <summary>
+    /// This method converts the skinned mesh to normal mesh.
+    /// </summary>
+    [ContextMenu("Convert to mesh")]
+    private void ConvertToMesh()
+    {
+        SkinnedMeshRenderer skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        meshFilter.sharedMesh = skinnedMeshRenderer.sharedMesh;
+        meshRenderer.sharedMaterials = skinnedMeshRenderer.sharedMaterials;
+
+        DestroyImmediate(skinnedMeshRenderer);
+        DestroyImmediate(this);
+    }
+
+}
