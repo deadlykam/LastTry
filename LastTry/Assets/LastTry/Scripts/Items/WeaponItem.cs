7017dd4519efd45ebecc05f085fc7eee6f2f84d0
Kamran Wali <kamran369mars@gmail.com>
2019-10-07 19:27:48 +0100

    Created WeaponItem
    This class extends the Items class. This is used for gameobjects that are weapons so that when picked up the player will get the weapon attributes.

---
+++ LastTry/Assets/LastTry/Scripts/Items/WeaponItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class handles all the weapon related events. Like picking up weapon, current weapon,
+/// enemy weapon and boss weapon. This class has replaced the previous class called 
+/// WeaponInfo.
+/// </summary>
+public class WeaponItem : Items
+{
+    public WeaponType Weapon = WeaponType.None;
+    public int DamageMax;
+    public int DamageMin;
+    public CombatAnimation[] AttackAnimations;
+
+    /// <summary>
+    /// This method returns a random damage value within the range.
+    /// </summary>
+    /// <returns>The random damage value within the range, of type int</returns>
+    public int GetDamage() { return Random.Range(DamageMin, DamageMax); }
+
+    /// <summary>
+    /// This method shows the description of the weapon.
+    /// </summary>
+    /// <returns>The description of the weapon, of type string</returns>
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ". Damage: " 
+               + DamageMin.ToString() + " - " 
+               + DamageMax.ToString();
+    }
+}
+
+public enum WeaponType { None, Sword, Staff, Hammer };
