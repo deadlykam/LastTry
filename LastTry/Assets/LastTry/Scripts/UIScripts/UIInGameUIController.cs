8b819b624f2a1039872878f539a448f96575fb40
Kamran Wali <kamran369mars@gmail.com>
2019-10-07 23:29:13 +0100

    Updated UIInGameUiController
    For now this class handles the weapon popup menu. For now the design of the class is to handle all the item pop menus.

--- LastTry/Assets/LastTry/Scripts/UIScripts/UIInGameUIController.cs
+++ LastTry/Assets/LastTry/Scripts/UIScripts/UIInGameUIController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class UIInGameUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIInGameUIController Instance;
+
+    public UIItemPopUpController WeaponItemPopup;
+
+    private void Awake()
     {
-        
+        // Condition for initializing the singleton
+        if (Instance == null)
+        {
+            Instance = this; // Singleton initialized
+            DontDestroyOnLoad(gameObject); // Not destroying on load
+        }
+        else Destroy(gameObject); // Already initialized so
+                                  // destroying
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// This method shows the weapon popup menu.
+    /// </summary>
+    /// <param name="item1Name">The name of the item 1, of type string</param>
+    /// <param name="item2Name">The name of the item 2, of type string</param>
+    /// <param name="item1Description">The description of item 1, of type string</param>
+    /// <param name="item2Description">The description of item 2, of type string</param>
+    public void ShowWeaponPopup(string item1Name, string item2Name,
+                         string item1Description, string item2Description)
     {
-        
+        // Showing the popup menu
+        WeaponItemPopup.ShowMenu(item1Name, item2Name, item1Description, item2Description);
     }
+
+    /// <summary>
+    /// This method hides the menu popup.
+    /// </summary>
+    public void HideWeaponPopup() { WeaponItemPopup.HideMenu(); }
+
+    /// <summary>
+    /// This method sets bar value for the Weapon Popup menu.
+    /// </summary>
+    /// <param name="amount">The fill amount for the popup bar in the range from
+    ///                      0 - 1, of type float</param>
+    public void SetWeaponBar(float amount) { WeaponItemPopup.SetFillAmount(amount); }
+
+    /// <summary>
+    /// This method checks if the weapon bar is not finished.
+    /// </summary>
+    /// <returns></returns>
+    public bool IsWeaponBarNotDone() { return WeaponItemPopup.IsBarNotFinished; }
 }
