4a2006beee3ad52179356cbab5ed2f1e40def80c
Kamran Wali <kamran369mars@gmail.com>
2019-10-08 20:32:17 +0100

    Created GameWorldManager
    This class is a singleton class. Will have links to common transform or gameobject that will be required by almost every other class. In the Start() method the instance is refered to the stage loaded so that it keeps refering to the current GameWorldManager.

---
+++ LastTry/Assets/LastTry/Scripts/Others/GameWorldManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWorldManager : MonoBehaviour
+{
+    public static GameWorldManager Instance;
+
+    public Transform Root;
+    public Transform Items;
+
+    private void Start()
+    {
+        // The instance will be replaced each time a new world is
+        // loaded so that basic
+        Instance = this;
+    }
+}
