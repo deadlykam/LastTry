d47d70ee852bf3cdbd3a1ce98a040d6558b704a7
Kamran Wali <kamran369mars@gmail.com>
2019-10-08 20:38:29 +0100

    Updated WeaponItems
    The weapon now keeps track of where the weapon should position and rotate when picked up. Also keeps track of how to be dropped in the gameworld.

--- LastTry/Assets/LastTry/Scripts/Items/WeaponItem.cs
+++ LastTry/Assets/LastTry/Scripts/Items/WeaponItem.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// This class handles all the weapon related events. Like picking up weapon, current weapon,
-/// enemy weapon and boss weapon. This class has replaced the previous class called 
-/// WeaponInfo.
+/// This class handles all the weapon related events. Like picking up weapon, 
+/// current weapon, enemy weapon and boss weapon. This class has replaced the previous
+/// class called WeaponInfo.
 /// </summary>
 public class WeaponItem : Items
 {
+    [Header("Weapon Item Properties")]
     public WeaponType Weapon = WeaponType.None;
     public int DamageMax;
     public int DamageMin;
     public CombatAnimation[] AttackAnimations;
+
+    [Header("Placement In Player Properties")]
+    public Vector3 PlacementPosition;
+    public Vector3 PlacementRotation;
+
+    [Header("Placement In World Properties")]
+    public Vector3 PositionOffset;
 
     /// <summary>
     /// This method returns a random damage value within the range.
@@ -29,6 +37,40 @@ public class WeaponItem : Items
         return base.GetDescription() + ". Damage: " 
                + DamageMin.ToString() + " - " 
                + DamageMax.ToString();
+    }
+
+    /// <summary>
+    /// This method sets the parent to the target and sets the placement position
+    /// and rotation.
+    /// </summary>
+    /// <param name="target">The new parent for the weapon item, of type Transform</param>
+    public void SetParentToPlayer(Transform target)
+    {
+        transform.parent = target;
+        transform.localPosition = PlacementPosition;
+        transform.localRotation = Quaternion.Euler(PlacementRotation);
+        SetCollider(false); // Removing collision trigger
+    }
+
+    /// <summary>
+    /// This method sets the parent to the target and sets the position offset.
+    /// </summary>
+    /// <param name="target">The new parent for the weapon item, of type Transform</param>
+    public void SetParentToWorld(Transform target)
+    {
+        transform.parent = target;
+
+        // Making y axis to 0
+        transform.position = new Vector3(transform.position.x,
+                                         0,
+                                         transform.position.z);
+        // Adding the new offset position
+        transform.position += PositionOffset;
+
+        // Making rotation to 0
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        SetCollider(true); // Enabling collision trigger
     }
 }
 
