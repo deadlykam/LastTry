83b255c78912e6553cefcf52af9e3e1d264e7be2
Kamran Wali <kamran369mars@gmail.com>
2019-10-09 15:04:27 +0100

    Created UIFollowCharacter
    This class makes a 3D UI follow a character and updates the health bar from the character's BasicCharacter.GetHealthPercentage()

---
+++ LastTry/Assets/LastTry/Scripts/UIScripts/UIFollowCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFollowCharacter : UIFollow
+{
+    public BasicCharacter Character;
+    public Image HealthBar;
+
+    private void Update()
+    {
+        // Calling the update method of the UI follow
+        UpdateUIFollow();
+
+        // Condition to check if health update is required
+        if (HealthBar.fillAmount != Character.GetHealthPercentage())
+        {
+            // Updating the health bar fill amount
+            HealthBar.fillAmount = Character.GetHealthPercentage();
+        }
+    }
+}
