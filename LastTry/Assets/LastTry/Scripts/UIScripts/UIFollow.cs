17d7edd1d81717b28736c5b16323b1d2572c6f85
Kamran Wali <kamran369mars@gmail.com>
2019-10-09 15:03:14 +0100

    Created UIFollow
    This class makes a 3D UI follow a target.

---
+++ LastTry/Assets/LastTry/Scripts/UIScripts/UIFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+
+    private void Update()
+    {
+        UpdateUIFollow();
+    }
+
+    /// <summary>
+    /// This method is the update method of UIFollow class.
+    /// </summary>
+    protected void UpdateUIFollow() { transform.position = Target.position + Offset; }
+}
