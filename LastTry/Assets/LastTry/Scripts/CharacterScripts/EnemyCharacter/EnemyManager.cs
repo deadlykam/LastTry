f01b3968ab2c4508fd148c099158e3de0085a43f
Kamran Wali <kamran369mars@gmail.com>
2019-10-09 15:06:56 +0100

    Updated EnemyManager
    This class now requests to give and release healthbar in behalf of the EnemyCharacter class.

--- LastTry/Assets/LastTry/Scripts/CharacterScripts/EnemyCharacter/EnemyManager.cs
+++ LastTry/Assets/LastTry/Scripts/CharacterScripts/EnemyCharacter/EnemyManager.cs
@@ -6,16 +6,26 @@ public class EnemyManager : MonoBehaviour
 {
     [Header("Enemy Manager Properties")]
     public PlayerCharacter Player;
+    public HealthBarPoolingManager HealthBarManager;
 
-    // Start is called before the first frame update
-    void Start()
+    
+    /// <summary>
+    /// This method requests an available health bar to be set.
+    /// </summary>
+    /// <param name="enemy">The enemy to set the healthbar to, of type
+    ///                     transform</param>
+    /// <returns>-1 means request failed, any other number means succeeded,
+    ///          of type int</returns>
+    public void RequestHealthBar(Transform enemy)
     {
-        
+        HealthBarManager.RequestHealthBar(enemy);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /// <summary>
+    /// This method requests to free up a health bar.
+    /// </summary>
+    /// <param name="index">The index of the health bar to be freed,
+    ///                     of type int</param>
+    public void RequestToReleasehealthBar(int index)
+    { HealthBarManager.RequestReleaseHealthBar(index); }
 }
