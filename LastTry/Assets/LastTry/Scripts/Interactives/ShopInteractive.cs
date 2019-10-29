f2a11f79131db5912383a85c262416f6a3cfef2c
Kamran Wali <kamran369mars@gmail.com>
2019-10-29 15:54:08 +0000

    Created ShopInteractive
    This is the shop interactive. In future this will open the shop menu and will handle the buying and selling of items.

---
+++ LastTry/Assets/LastTry/Scripts/Interactives/ShopInteractive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopInteractive : InteractiveController
+{
+
+    /// <summary>
+    /// This method opens the shop.
+    /// </summary>
+    public override void Action()
+    {
+        // Todo: Open shop UI here
+        // Todo: Load player equipments here
+        UIShopController.Instance.ShowMenu();
+    }
+
+    /// <summary>
+    /// This method gets the description of the shop.
+    /// </summary>
+    /// <returns>The description of the shop, of type string</returns>
+    public override string GetDescription()
+    {
+        return "To open " + InteractiveName;
+    }
+}
