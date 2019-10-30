e09d5b91ac2b0e342d7b14de95aa855218a59f73
Kamran Wali <kamran369mars@gmail.com>
2019-10-30 16:02:49 +0000

    Update Items.cs
    Shifted common virtual methods to the parent class InteractiveController.

--- LastTry/Assets/LastTry/Scripts/Items/Items.cs
+++ LastTry/Assets/LastTry/Scripts/Items/Items.cs
@@ -2,32 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
-public class Items : MonoBehaviour
+public class Items : InteractiveController
 {
     [Header("Items Properties")]
-    public string ItemName;
     public int Cost;
-    public BoxCollider ItemCollider;
 
     [Header("Placement In World Properties")]
     public Vector3 PositionOffset;
-
-    /// <summary>
-    /// This method decides if the collider will be enabled or disabled.
-    /// </summary>
-    /// <param name="isEnable">The flag to enable or disable the item collider,
-    ///                        of type BoxCollider</param>
-    protected virtual void SetCollider(bool isEnable) { ItemCollider.enabled = isEnable; }
-
-    /// <summary>
-    /// This method returns the description of the item.
-    /// </summary>
-    /// <returns>The description of the item, of type string</returns>
-    public virtual string GetDescription()
-    {
-        return ItemName;
-    }
 
     /// <summary>
     /// This method drops the item in the target transform.
